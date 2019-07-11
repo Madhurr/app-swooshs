413a02497e1fb4f3f84a8b29df01ac61a9b96a19
madhur_vkgp <madhur@xamidea.in>
2019-07-11 17:49:33 +0530

    ÄutoLayout_start

--- Swoosh/ViewController.swift
+++ Swoosh/ViewController.swift
@@ -10,9 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var swoosh: UIImageView!
+    @IBOutlet var bgImg: UIImageView!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //Coardinate System in ios start with (0,0)
+        swoosh.frame = CGRect(x: view.frame.size.width / 2 - swoosh.frame.size.width/2, y: 50, width: swoosh.frame.width, height: swoosh.frame.height)
+        bgImg.frame = view.frame
+       
     }
 
 
