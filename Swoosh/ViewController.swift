b368de26e8571b1596a053f528afb21272457087
madhur_vkgp <madhur@xamidea.in>
2019-07-15 15:54:35 +0530

    Üi_Completed_with_constraint

--- Swoosh/ViewController.swift
+++ Swoosh/ViewController.swift
@@ -18,8 +18,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         //Coardinate System in ios start with (0,0)
-        swoosh.frame = CGRect(x: view.frame.size.width / 2 - swoosh.frame.size.width/2, y: 50, width: swoosh.frame.width, height: swoosh.frame.height)
-        bgImg.frame = view.frame
+        
        
     }
 
